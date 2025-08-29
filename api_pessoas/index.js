c003f7c5e22d0ef7dc5cf5a6114448b53a57cbcd
Guilherme Pires <guilhermeemanuel45@gmail.com>
2025-08-28 22:06:38 -0300

    api pessoas

---
+++ api_pessoas/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+const pessoas = [];
+var countID = 0;
+
+app.get("/", (request, response) => {
+    return response.send(pessoas);
+});
+
+app.post("/cadastro", (request, response) => {
+    const nome = request.body.nome;
+    const telefone = request.body.telefone;
+
+    pessoas.push({
+        id: countID++,
+        nome,
+        telefone
+    });
+
+    return response.send({ nome, telefone });
+});
+
+app.get("/listar:nome", (request, response) => {
+    const { nome } = request.params;
+    return response.send({nome});
+})
+
+app.listen(8080, () => {
+    console.log("O servidor está rodando na porta 8080");
+});
+
+// http://localhost:8080    
