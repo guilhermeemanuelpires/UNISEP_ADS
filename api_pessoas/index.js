fdf7d9d0474975ab0fc836aab566452bcf99dac8
Guilherme Pires <guilhermeemanuel45@gmail.com>
2025-09-04 21:41:30 -0300

    api pessoas

--- api_pessoas/index.js
+++ api_pessoas/index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 const pessoas = [];
-var countID = 0;
+var countID = 1;
 
 app.get("/", (request, response) => {
     return response.send(pessoas);
@@ -24,13 +24,44 @@ app.post("/cadastro", (request, response) => {
     return response.send({ nome, telefone });
 });
 
-app.get("/listar", (request, response) => {
+app.get("/listar/:nome", (request, response) => {
     const { nome } = request.params;
-    return response.send({nome});
-})
+
+    const pessoa = pessoas.filter((i) => i.nome === nome);
+
+    return response.send(pessoa);
+});
+
+app.put("/atualizar/:id", (request, response) => {
+    const { id } = request.params;
+    const { nome, telefone } = request.body;
+
+    const pessoaIndex = pessoas.findIndex((i) => i.id == id);
+
+    if (pessoaIndex <= -1) {
+        return response.send({ msg: "O código da pessoa não existe!" });
+    }
+
+    pessoas[pessoaIndex].nome = nome;
+    pessoas[pessoaIndex].telefone = telefone;
+
+    return response.send({ nome, telefone });
+});
+
+app.delete("/deletar/:id", (request, response) => {
+    const { id } = request.params;
+
+    const pessoasIndex = pessoas.findIndex(i => i.id == id);
+
+    if(pessoasIndex <= -1){
+        return response.send({msg : "O Código da pessoa está inválido!"});
+    }else{
+        pessoas.splice(pessoasIndex,1);
+    }    
+
+    return response.send({msg : "Pessoa deletada com sucesso!"});
+});
 
 app.listen(8080, () => {
     console.log("O servidor está rodando na porta 8080");
 });
-
-// http://localhost:8080    
